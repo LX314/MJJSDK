b014a0564cdd12012fb73ad28a81d63a556103a3
LX314 <1094426094@qq.com>
2016-05-23 18:42:18 +0800

    Initial commit

---
+++ Example/MJJSDK/MJViewController.h
@@ -0,0 +1,13 @@
+//
+//  MJViewController.h
+//  MJJSDK
+//
+//  Created by LX314 on 05/23/2016.
+//  Copyright (c) 2016 LX314. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MJViewController : UIViewController
+
+@end
